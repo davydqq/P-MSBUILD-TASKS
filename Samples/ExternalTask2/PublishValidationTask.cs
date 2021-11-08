93320362b14b53886d365e49b53f5950fff266db
Davyd Konopatskyi <davydqq@gmail.com>
2021-11-08 21:22:13 +0200

    init

---
+++ Samples/ExternalTask2/PublishValidationTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Build.Framework;
+
+namespace ExternalTask2
+{
+    public class PublishValidationTask : Microsoft.Build.Utilities.Task
+    {
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, "Hello World TEST PublishValidationTask");
+            return true;
+        }
+    }
+}
